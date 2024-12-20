97885c82da58bfda04c9914821b67fd100a74e7c
pavithra <parameshwaripavithra22@gmail.com>
2024-12-20 09:46:10 +0530

    Remove files pushed accidentally

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-export default defineConfig({
-  // base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
-  plugins: [react()],
-  base: '/portfolio/', // Replace with your repo name
-  
-});
