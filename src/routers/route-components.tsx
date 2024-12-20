fd73947f4754d40cc84f402ca680b51e8b8727db
pavithra <parameshwaripavithra22@gmail.com>
2024-12-20 09:51:54 +0530

    Initial commit

---
+++ src/routers/route-components.tsx
@@ -0,0 +1,17 @@
+
+import Sidebar from "../components/portfolio/sidebar/sidebar";
+import Home from "../components/portfolio/home/home";
+import About from "../components/portfolio/about/about";
+import Resume from "../components/portfolio/resume/resume";
+import ContactForm from "../components/portfolio/contact/contact";
+import Dashboard from "../components/portfolio/dashboard/dashboard";
+
+export{
+ 
+   Sidebar,
+   Home,
+   About,
+   Resume,
+   ContactForm,
+   Dashboard
+}
